af1fe6738b06e5667a230ff43f552f7385037518
ymyang <ymyang@163.com>
2015-07-28 09:13:56 +0800

    fis-conf

--- fis-conf.js
+++ fis-conf.js
@@ -1,37 +1,128 @@
-/**
- * Created by yang on 2015/7/10.
- */
-// 设置图片合并的最小间隔
-fis.config.set('settings.spriter.csssprites.margin', 20);
-fis.config.set('roadmap.path', [{
-    reg: '**.css',
-    useSprite: true
-}]);
+fis.config.merge({
+    urlPrefix: '',
 
-//如果要兼容低版本ie显示透明png图片，请使用pngquant作为图片压缩器，
-//否则png图片透明部分在ie下会显示灰色背景
-//使用spmx release命令时，添加--optimize或-o参数即可生效
-//fis.config.set('settings.optimzier.png-compressor.type', 'pngquant');
+    project: { charset: 'utf8'},
 
-//设置jshint插件要排除检查的文件，默认不检查lib、jquery、backbone、underscore等文件
-//使用pure release命令时，添加--lint或-l参数即可生效
-fis.config.set('settings.lint.jshint.ignored', [ 'lib/**', /jquery|backbone|underscore/i ]);
+    roadmap: {
+        path: [
+            {
+                reg: /^\/app\/components\/(.*)\/([\w\-_\.]*)\.(js)$/i,
+                isMod: true,
+                useSprite: false,
+                useOptimizer: false,
+                id: '$2'
+            },
+            {
+                reg: /^\/app\/js\/(.*)\.(js|coffee)$/i,
+                //是组件化的，会被jswrapper包装
+                isMod: true,
+                useSprite: false,
+                isAngular: true,
+                //id是去掉sea-modules和.js后缀中间的部分
+                id: '$1',
+                //url: '/yliyun/sea-modules/app/$1.$2',
+                //release: '/app/js/$1.$2'
+            },
+            {
+                //.mixin.less后缀的文件
+                reg: /\.mixin\.less$/,
+                //仅当做函数调用，不发布
+                release: false
+            },
+            {
+                //其他js、css、coffee、less文件
+                reg: /\.(js|coffee|css|less)$/,
+                //less和css文件会做csssprite处理
+                useSprite: true,
+                //不要放到js资源表里
+                useMap: false
+            },
+            {
+                //sea-modules目录下的其他文件
+                reg: /^\/app\/js\/(.*)\.tpl$/i,
+                isMod: false,
+                useSprite: false,
+                isHbsFile: true,
+                isHtmlLike: true,
+                release: false
+            },
+            {
+                reg: /^\/app\/pages\/(.*)\.html/i,
+                useMap: false,
+                useCache: false
+                //release: '/app/$1.html'
+            },
+            {
+                //前端模板
+                reg: '**.tmpl',
+                //当做类js文件处理，可以识别__inline, __uri等资源定位标识
+                isJsLike: true,
+                //只是内嵌，不用发布
+                release: false
+            }
+        ],
+        ext: {
+            //less输出为css文件
+            less: 'css',
+            //coffee输出为js文件
+            coffee: 'js',
+            // tpl 编译为js模版函数
+            tpl: 'js'
+        }
+    },
 
-// 开启simple插件
-fis.config.set('modules.postpackager', 'simple');
+    modules: {//fis插件配置
+        parser: {
+            //.tmpl后缀的文件使用fis-parser-utc插件编译
+            tmpl: 'utc',
+            //.coffee后缀的文件使用fis-parser-coffee-script插件编译
+            coffee: 'coffee-script',
+            //.less后缀的文件使用fis-parser-less插件编译
+            less: 'less'
+        },
 
-// 设置打包规则
-fis.config.set('pack', {
-    '/pkg/lib.js': [
-        'js/lib/jquery.js',
-        'js/lib/underscore.js',
-        'js/lib/backbone.js',
-        'js/lib/backbone.localStorage.js',
-    ],
-    // 设置CSS打包规则，CSS打包的同时会进行图片合并
-    '/pkg/aio.css': '**.css'
+        postprocessor: {
+            js: ['jswrapper', 'require-async']
+        },
+
+        lint: {
+            js: 'jshint'
+        },
+
+        postpackager: ['modjs', 'autoload', 'simple'],
+
+        optimizer: {
+            js: ['ng-annotate', 'uglify-js'],
+            css: 'clean-css',
+            png: 'png-compressor'
+        }
+    },
+
+    settings: {
+        parser: {
+            'coffee-script': {
+                //不用coffee-script包装作用域
+                bare: true
+            }
+        },
+        //lint: {
+        //    jshint: {
+        //        //排除对lib和jquery、backbone、underscore的检查
+        //        ignored: ['app/components/**', /jquery|backbone|underscore|\$|bootstrap/i],
+        //        //使用中文报错
+        //        i18n: 'zh-CN'
+        //    }
+        //},
+        postprocessor: {
+            jswrapper: {
+                //用fis的js包装器，更方便书写
+                type: 'amd'
+            }
+        },
+        postpackager: {
+            modjs: {
+                subpath: 'pack/map.js'
+            }
+        }
+    }
 });
-
-fis.config.set('modules.preprocessor.js', 'annotate');
-// 开启simple对零散资源的自动合并
- fis.config.set('settings.postpackager.simple.autoCombine', true);
